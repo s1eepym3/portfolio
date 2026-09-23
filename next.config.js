07a673fc328fcbae574f150c4223ee05a92525c1
hykl <mohdhaykhal67@gmail.com>
2026-09-24 00:50:31 +0700

    chore: remove GitHub Pages config and workflows

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/portfolio', 
-  images: {
-    unoptimized: true,
-  },
 };
 
 module.exports = nextConfig;
